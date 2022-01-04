9da730463f5b9698a2a0ed31e8efd52618740019
Piotr <piotrjgasiorek@gmail.com>
2022-01-04 12:32:08 +0100

    Added results section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Results from './components/Results';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Results/>
     </div>
   );
 }
