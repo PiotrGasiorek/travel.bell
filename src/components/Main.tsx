f629dfac758695a4c7505000bfd7e93a9a926bf4
Piotr <piotrjgasiorek@gmail.com>
2022-01-13 17:49:57 +0100

    Added message component for search results feedback

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,19 @@
 import CardsList from './CardsList';
+import Message from './Message';
 import Form from './Form';
 import { useAppSelector } from '../app/hooks';
-
 
 function Main() {
   const { results, rating } = useAppSelector(state => state.search);
   const filteredHotels = results.filter(hotel => hotel.starRating <= rating);  
+  const isDataFromApiAvailable = results.length !== 0;
+  const noHotelsAvailable = filteredHotels.length === 0;
   return (
     <main className='main'>
       <Form/>
-      {filteredHotels.length === 0 && 'Sorry we could not find any hotels which would fit your requirements'}
+      {(noHotelsAvailable && isDataFromApiAvailable) && 
+        <Message text='Sorry we could not find any hotels which would fit your requirements'/>
+      }
       <CardsList hotels={filteredHotels}/>
     </main>
   );
